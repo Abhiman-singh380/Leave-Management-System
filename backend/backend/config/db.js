f38cdcad3e97efadf3c45028ff77580f1dcaf903
Abhiman Singh <your-email@gmail.com>
2026-06-10 12:49:21 +0530

    Fix MongoDB Atlas connection

--- backend/backend/config/db.js
+++ backend/backend/config/db.js
@@ -7,15 +7,3 @@ const connectDB = async () => {
 
 module.exports = connectDB;
 
-
-
-
-
-// const mongoose = require("mongoose");
-
-// const connectDB = async () => {
-//   await mongoose.connect("mongodb://127.0.0.1:27017/leaveDB");
-//   console.log("MongoDB Connected");
-// };
-
-// module.exports = connectDB;
